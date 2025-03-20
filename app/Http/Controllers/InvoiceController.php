3f436f75d2eb07eeee090678636a42d3f677b0b0
ashikurweb <ashikurrahman7194@gmail.com>
2025-03-20 11:12:29 +0600

    Invoices Searching and Filtering

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -9,9 +9,33 @@ class InvoiceController extends Controller
 {
     public function get_all_invoice()
     {
-        $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->paginate(8);
+        // $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->paginate(8);
+        // return response()->json($invoices, 200);
 
-        return response()->json($invoices, 200);
+        $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+        return response()->json([
+            'invoices'  => $invoices
+        ], 200);
     }
+
+
+    public function search_invoice(Request $request)
+    {
+        $search = $request->get('s');
+        if ($search) {
+            $invoices = Invoice::with('customer')
+            ->whereHas('customer', function($query) use ($search) {
+                $query->where('first_name', 'like', "%$search%");
+            })
+            ->get();
+
+            return response()->json([
+                'invoices'  => $invoices
+            ], 200);
+        } else {
+            return $this->get_all_invoice();
+        }
+    }
+
 
 }
